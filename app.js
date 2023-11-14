f0a28b5283879969438cb1897117910d92ccfdc5
John P. Jones II <jpauljonesii@gmail.com>
2023-11-14 12:58:09 -0600

    create files and add json data

---
+++ app.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const quotesArray = [
+  {
+    content: 'All action results from thought, so it is thoughts that matter.',
+    author: 'Sai Baba',
+    tags: ['Famous Quotes'],
+    authorSlug: 'sai-baba',
+    length: 63,
+    dateAdded: '2020-02-22',
+    dateModified: '2023-04-14',
+  },
+
+  {
+    content: 'Friends are the siblings God never gave us.',
+    author: 'Mencius',
+    tags: ['Friendship'],
+    authorSlug: 'mencius',
+    length: 43,
+    dateAdded: '2021-03-26',
+    dateModified: '2023-04-14',
+  },
+
+  {
+    content:
+      'Follow your instincts. That is where true wisdom manifests itself.',
+    author: 'Oprah Winfrey',
+    tags: ['Famous Quotes'],
+    authorSlug: 'oprah-winfrey',
+    length: 66,
+    dateAdded: '2020-12-17',
+    dateModified: '2023-04-14',
+  },
+];
